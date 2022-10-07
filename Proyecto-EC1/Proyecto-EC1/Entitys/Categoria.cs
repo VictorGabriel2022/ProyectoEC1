1bb2d1d3f26f42dc55ca96f9f153e7bd6aad0991
Javichoca <114101111+Javichoca@users.noreply.github.com>
2022-10-07 13:11:33 -0500

    Add files via upload

---
+++ Proyecto-EC1/Proyecto-EC1/Entitys/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+
+namespace Proyecto_EC1.Entitys
+{
+    public class Categoria
+    {
+        [Key]
+        public int ID_CATG { get; set; }
+        [Required]
+        //definimos el tamaño del campo
+        [StringLength(maximumLength: 100,
+            ErrorMessage = "Se tiene que ingresar un nombre de un producto")]
+        public string NOM_CATG { get; set; }
+        [Required]
+        public bool DESP_CATG { get; set; }
+        [Required]
+        public Producto producto { get; set; }
+    }
+}
--- Proyecto-EC1/Proyecto-EC1/Entitys/Producto.cs
+++ Proyecto-EC1/Proyecto-EC1/Entitys/Producto.cs
@@ -1,18 +1,36 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
 
 namespace Proyecto_EC1.Entitys
 {
     public class Producto
     {
+        internal int ID;
+
+        //definimos la clave primaria
         [Key]
-        public int Id_Producto { get; set; }
+        public int ID_PROD { get; set; }
+        //definimos valores no nulos
         [Required]
         //definimos el tamaño del campo
-        [StringLength(maximumLength: 100,
-            ErrorMessage = "Se tiene que ingresar un nombre")]
-        public string titulo { get; set; }
+        [StringLength(
+            maximumLength: 100,
+            ErrorMessage = "Se tiene que ingresar un nombre de un producto"
+            )]
+        public string ID_CATG { get; set; }
         [Required]
-        public bool estado { get; set; }
-      
+        public bool NOM_PROD { get; set; }
+
+        [Required]
+        public bool PREC_PROD { get; set; }
+
+        [Required]
+        public bool STOCK_PROD { get; set; }
+
+
+
+        public List<Categoria> Categoria { get; set; }
     }
 }
