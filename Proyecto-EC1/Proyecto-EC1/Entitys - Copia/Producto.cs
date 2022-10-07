c86b4fbf76c77ce92aeabacccab9343f4c2add7d
Lisbethpatriciatuctoobando14 <lisbethpatriciatuctoobando@gmail.com>
2022-10-06 21:47:43 -0500

    Base de datos
    Creación de tablas y relaciones

---
+++ Proyecto-EC1/Proyecto-EC1/Entitys - Copia/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_EC1.Entitys
+{
+    public class Producto
+    {
+        [Key]
+        public int Id_Producto { get; set; }
+        [Required]
+        //definimos el tamaño del campo
+        [StringLength(maximumLength: 100,
+            ErrorMessage = "Se tiene que ingresar un nombre")]
+        public string titulo { get; set; }
+        [Required]
+        public bool estado { get; set; }
+      
+    }
+}
