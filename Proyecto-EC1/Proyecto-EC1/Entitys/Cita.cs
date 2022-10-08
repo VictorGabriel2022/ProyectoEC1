bcfa7a33ed8128b0d76fc1c529392725a575cf04
bsm97 <102140022+bsm97@users.noreply.github.com>
2022-10-07 20:08:31 -0500

    Add files via upload

---
+++ Proyecto-EC1/Proyecto-EC1/Entitys/Cita.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_EC1.Entitys
+{
+    public class Cita
+    {
+        [Key]
+        public int id_cita { get; set; }
+        [Required]
+        //definimos el tamaño del campo
+        [StringLength(maximumLength: 100,
+            ErrorMessage = "Se tiene que ingresar un nombre")]
+        public string distrito { get; set; }
+        [Required]
+        public string direccion { get; set; }
+        public DateAndTime fecha { get; set; }
+      
+    }
+}
---
+++ Proyecto-EC1/Proyecto-EC1/Entitys/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_EC1.Entitys
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id_Cliente { get; set; }
+        [Required]
+        //definimos el tamaño del campo
+        [StringLength(maximumLength: 100,
+            ErrorMessage = "Se tiene que ingresar un nombre")]
+        public string nombre { get; set; }
+        [Required]
+        public string apellido { get; set; }
+        public DateAndTime fech_nac { get; set; }
+        public int ruc { get; set; } 
+        public int tip_doc { get; set; }
+
+    }
+}
