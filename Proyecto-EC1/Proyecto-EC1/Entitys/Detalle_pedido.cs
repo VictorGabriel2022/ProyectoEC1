7d5a92a28cc1f99cc7521357efeb39b72a3c8726
Schyal <114102026+Schyal@users.noreply.github.com>
2022-10-07 20:50:26 -0500

    Add files via upload

---
+++ Proyecto-EC1/Proyecto-EC1/Entitys/Detalle_pedido.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_EC1.Entitys
+{
+    public class Detalle_pedido
+    {
+        //definimos la clave primaria
+        [Key]
+        public int Id_dped { get; set; }
+        //definimos valores no nulos
+        [Required]
+        //definimos el tamaño del campo
+        [StringLength(
+        maximumLength: 100,
+        ErrorMessage = "Se tiene que ingresar un pedido"
+        )]
+        public int Id_pedido { get; set; }
+        
+        public int Id_prod { get; set; }
+
+        public string producto { get; set; }
+        [Required]
+        public int cant { get; set; }
+        [Required]
+        public int und { get; set; }
+
+        public float precio { get; set; }
+
+        public float subtotal { get; set; }
+
+    }
+}
---
+++ Proyecto-EC1/Proyecto-EC1/Entitys/Pedido.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_EC1.Entitys
+{
+    public class Pedido
+    {
+        //definimos la clave primaria
+        [Key]
+        public int Id_pedido { get; set; }
+        //definimos valores no nulos
+        [Required]
+        //definimos el tamaño del campo
+        [StringLength(
+        maximumLength: 100,
+        ErrorMessage = "Se tiene que ingresar un pedido"
+        )]
+        public string Id_cliente { get; set; }
+        [Required]
+        public string Id_factura { get; set; }
+
+        public DateAndTime fech_ped { get; set; }
+
+        public DateAndTime hor_ped { get; set; }
+
+        public DateAndTime hor_entr { get; set; }
+
+        public int num_ped { get; set; }
+
+        public DateAndTime fech_entr { get; set; }
+
+        public string obs { get; set; }
+
+
+
+
+    }
+}
